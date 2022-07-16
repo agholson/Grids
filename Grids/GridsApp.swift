2218a31584bfcc4aec9e9a2524509c1f5615eff8
agholson <87455899+agholson@users.noreply.github.com>
2022-07-16 14:13:12 -0400

    Initial Commit

---
+++ Grids/GridsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridsApp.swift
+//  Grids
+//
+//  Created by Leone on 7/16/22.
+//
+
+import SwiftUI
+
+@main
+struct GridsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
